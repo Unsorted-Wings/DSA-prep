3d7b2ab5d6909ac477d3f50069b465a9ba4edce9
rohit shukla <shuklarohit2105@gmail.com>
2024-01-02 22:53:54 +0530

    more on recursion

--- day_1/recursion.cpp
+++ day_1/recursion.cpp
@@ -2,15 +2,64 @@
 
 using namespace std;
 
-void funct(int i, int n)
-{
-    if(i > n) return;
+//printing name n times
 
+/*
+void funct(int i, int n){
+    if(i > n) return;
+    cout << "rohit";
+    funct(i+1,n);
+}
+*/
+
+//printing number from 1 to n
+
+/*
+void funct(int i, int n){
+    if(i > n) return;
     cout << i << endl;
     funct(i+1, n);
 }
+*/
 
-int main()
-{
-    funct(1, 5);
+//printing number from n to 1
+
+/*
+void funct(int i, int n){
+    if(i < n) return;
+    cout << i;
+    funct(i-1,n);
+}
+*/
+
+//printing number from 1 to n (backtrack)
+/*
+void funct(int i){
+    if(i < 1) return;
+    
+    funct(i-1);
+    cout << i << endl;
+}
+*/
+
+//printing number from n to 1 (backtrack)
+void funct(int i, int n){
+    if(i > n) return;
+    funct(i+1,n);
+    cout << i;
+}
+
+int main(){
+    //for name
+    //funct(1, 5);
+
+    //for printing from t to n
+    //funct(1, 5);
+
+    //for printing from n to 1
+    //funct(5,5);
+
+    //for printing from t to n (backtrack)
+    //funct(5);
+
 }
